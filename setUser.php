8ae36e59de0f49cacf74d2848e5cca3a5b0329ed
namblu <vunam3445@gmail.com>
2024-04-04 20:58:44 +0700

    new file:   setUser.php

---
+++ setUser.php
@@ -0,0 +1,36 @@
+<?php
+include("./Connect.php");
+$db = new Database();
+if(!isset($_REQUEST['userid']) || !isset($_REQUEST['fullName']) || !isset($_REQUEST['password']) || !isset($_REQUEST['avatar_image'])){
+    $response['status']=false;
+    $response['message']='error';
+}else{
+    $userid = $_REQUEST['userid'];
+    $fullName = $_REQUEST['fullName'];
+    $password = $_REQUEST['password'];
+    $avatar_image = $_REQUEST['avatar_image'];
+    if($userid=='' || $fullName=='' || $password=='' || $avatar_image==''){
+        $response['status']=false;
+        $response['message']='error';
+    }else{
+        $sql = "update users set fullName = '".$fullName."', password = '".$password."', avatar_image = '".$avatar_image."' where userid = ".$userid;
+        $result = $db->query($sql);
+        if($result){
+            $response['status']=true;
+            $response['data']=array(
+                'userid'=>$userid,
+                'fullName'=>$fullName,
+                'password'=>$password,
+                'avatar_image'=>$avatar_image
+            );
+        }else{
+            $response['status']=false;
+            $response['message']='error';
+        }
+        
+    }
+}
+echo json_encode( $response );
+$db->close();
+
+?>
