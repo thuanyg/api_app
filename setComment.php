15cffce7e387d806c023841542a73ab6dc8a17e4
namblu <vunam3445@gmail.com>
2024-04-05 21:43:26 +0700

    modified:   setComment.php 	modified:   setFavouritePost.php

--- setComment.php
+++ setComment.php
@@ -9,24 +9,30 @@ if(!isset($_REQUEST['userid']) || !isset($_REQUEST['content']) || !isset($_REQUE
     $post_id = $_REQUEST['post_id'];
     $content = $_REQUEST['content'];
     $date = $_REQUEST['date'];
-    $sql = "insert into comment_post (userid,post_id,content,date) values(".$userid.",".$post_id.",'".$content."','".$date."')";
-    $result = $db->query($sql);
-    if($result){
-        $sql = "select u.fullName,u.avatar_image from users u where userid = ".$userid;
-        $result1 = $db->query($sql);
-        if($row = $result1->fetch_assoc()){
-            $user = array(
-            'userid' => $userid,
-            'fullName' => $row['fullName'],
-            'content' => $content,
-            'post_id' => $post_id,
-            'avatar_image' => $row['avatar_image']
-            );
+    if($userid=="" || $post_id=="" || $content=="" || $date==""){
+        $response['status'] = false;
+        $response['message']= "error data";
+    }else{
+        $sql = "insert into comment_post (userid,post_id,content,date) values(".$userid.",".$post_id.",'".$content."','".$date."')";
+        $result = $db->query($sql);
+        if($result){
+            $sql = "select u.fullName,u.avatar_image from users u where userid = ".$userid;
+            $result1 = $db->query($sql);
+            if($row = $result1->fetch_assoc()){
+                $user = array(
+                'userid' => $userid,
+                'fullName' => $row['fullName'],
+                'content' => $content,
+                'post_id' => $post_id,
+                'avatar_image' => $row['avatar_image']
+                );
+            }
+        
         }
-    
+        $response['status'] = true;
+        $response['data'] = $user;
     }
-    $response['status'] = true;
-    $response['data'] = $user;
+    
 }
 echo json_encode($response);
 $db->close();
