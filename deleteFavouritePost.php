2da178d43502dead8f468f3881cab95779de49bb
thuanyghg <thuan2682k3@gmail.com>
2024-04-06 15:29:34 +0700

    modified:   deleteComment.php modified:   deleteFavouritePost.php modified:   getComments.php modified:   getFavouritePost.php modified:   getPostsOfCategory.php modified:   getUser.php modified:   searchPosts.php modified:   setComment.php modified:   setFavouritePost.php 	modified:   setUser.php

--- deleteFavouritePost.php
+++ deleteFavouritePost.php
@@ -1,5 +1,13 @@
 <?php
 include('./Connect.php');
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
 $db = new Database();
 if(!isset($_REQUEST['favourite_id'])){
     $response['status']=false;
--- getPostsOfCategory.php
+++ getPostsOfCategory.php
@@ -1,5 +1,13 @@
 <?php
 include "./Connect.php";
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
 $db = new Database();
 $num_row = 10; // Số bài viết trên mỗi trang
 
