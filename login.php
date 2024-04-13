567501c91ec62abed8b66eb0eaa00b19f8626c14
namblu <vunam3445@gmail.com>
2024-04-13 22:33:20 +0700

    modified:   login.php

--- login.php
+++ login.php
@@ -5,27 +5,26 @@ $db = new Database();
 if (isset($_REQUEST["email"]) && isset($_REQUEST["password"])) {
     $email = $_REQUEST["email"];
     $password = ($_REQUEST['password']);
-    if($email=="" || $password==""){
+    if ($email == "" || $password == "") {
         $response['status'] = false; // Đánh dấu trạng thái lỗi
         $response['message'] = 'error data'; // Thêm thông điệp lỗi
-    }else{
-        $sql = "select * from users where email ='" . $email . "' and password ='" . $password ."'";
+    } else {
+        $sql = "select userid from users where email ='" . $email . "' and password ='" . $password . "'";
         $result = $db->query($sql);
-    
+
         if ($row = $result->fetch_assoc()) {
+
             $response['status']  = true;
-        $response['message'] = "Login Successfully!";
-        }else{
+            $response['data'] = $row['userid'];
+            $response['message'] = "Login Successfully!";
+        } else {
             $response['status']  = false;
-        $response['message'] = "No results found";
+            $response['message'] = "No results found";
         }
-        
     }
-    
 } else {
     $response['status'] = false; // Đánh dấu trạng thái lỗi
     $response['message'] = 'No found param';
 }
 echo json_encode($response);
 $db->close();
-?>
